ff35767b075189330b0307de7d810a6bad3b88c0
azurexander <azaleabursts@gmail.com>
2016-07-06 11:52:55 -0700

    Add latitude and longitude columns for TimeAndPlace

--- app/models/time_and_place.rb
+++ app/models/time_and_place.rb
@@ -1,2 +1,8 @@
 class TimeAndPlace < ActiveRecord::Base
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    street_address + " " + city + " " + state + " " + zip_code
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160706180821) do
-
+ActiveRecord::Schema.define(version: 20160706184257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +25,8 @@ ActiveRecord::Schema.define(version: 20160706180821) do
     t.string   "zip_code"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
-
 
 end
