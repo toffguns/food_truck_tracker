e9c91ac89d83e10fe3b0ef6715ee9831d47b46b4
mosclov <federico@grupofemal.com>
2016-07-07 14:27:43 -0700

    Owner views are created

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,81 @@
+class OwnersController < ApplicationController
+  before_action :set_owner, only: [:show, :edit, :update, :destroy]
+  # before_action :check_auth
+  skip_before_action :authenticate_owner!
+
+  # def check_auth
+  #   unless owner_signed_in?
+  #     redirect_to landing_page_index_path
+  #   end
+  # end
+  # GET /owners
+  # GET /owners.json
+  def index
+    @owners = Owner.all
+  end
+
+  # GET /owners/1
+  # GET /owners/1.json
+  def show
+  end
+
+  # GET /owners/new
+  def new
+    @owner = Owner.new
+  end
+
+  # GET /owners/1/edit
+  def edit
+  end
+
+  # POST /owners
+  # POST /owners.json
+  def create
+    @owner = Owner.new(owner_params)
+
+    respond_to do |format|
+      if @owner.save
+        format.html { redirect_to @owner, notice: 'Owner was successfully created.' }
+        format.json { render :show, status: :created, location: @owner }
+      else
+        format.html { render :new }
+        format.json { render json: @owner.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /owners/1
+  # PATCH/PUT /owners/1.json
+  def update
+    respond_to do |format|
+      if @owner.update(owner_params)
+        format.html { redirect_to @owner, notice: 'Owner was successfully updated.' }
+        format.json { render :show, status: :ok, location: @owner }
+      else
+        format.html { render :edit }
+        format.json { render json: @owner.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /owners/1
+  # DELETE /owners/1.json
+  def destroy
+    @owner.destroy
+    respond_to do |format|
+      format.html { redirect_to owners_url, notice: 'Owner was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_owner
+      @owner = Owner.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def owner_params
+      params.fetch(:owner, {})
+    end
+end
