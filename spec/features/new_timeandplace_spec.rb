8dd01de8d6fc5236953782766be5a186cbfc05d0
azurexander <azaleabursts@gmail.com>
2016-07-07 10:26:39 -0700

    fixed failing login tests"

--- spec/features/new_timeandplace_spec.rb
+++ spec/features/new_timeandplace_spec.rb
@@ -6,8 +6,15 @@ RSpec.feature "TimeAndPlace", type: :feature do
       Given 'I go to the new TimeAndPlace for' do
         visit 'time_and_places/new'
       end
+      When 'I fill in email and password fields' do
+        click_link 'Sign up'
+        fill_in 'Email', with: 'food02@gmail.com'
+        fill_in 'Password', with: 'federico'
+        fill_in 'Password confirmation', with: 'federico'
+        click_button 'Sign up'
+      end
       Then 'I can fill in the form' do
-        fill_in 'Street address', with: "12345 main street"
+        fill_in 'time_and_place_street_address', with: "12345 main street"
         fill_in 'City', with: "San Diego"
         fill_in 'Zip code', with: "12345"
         fill_in 'State', with: "CA"
