b7d5269a45edfdccdf4975adeb6074825a134ece
learn <learn@learn11.local>
2016-07-11 11:37:08 -0700

    Add test for contributors

---
+++ spec/features/contributors_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Contributors", type: :feature do
+  context 'logging in' do
+    Steps 'logging in as a contributor' do
+      Given 'I am  on the login page' do
+        visit '/'
+        click_link 'Login'
+      end
+    end
+  end
+end
