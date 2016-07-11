108b5da054ac79b77b6c62fb7c7eca7bdc3d9842
learn <learn@learn11.local>
2016-07-11 14:22:21 -0700

    Add owner profile page

---
+++ spec/features/owners_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Owners", type: :feature do
+  context 'logging in and profile page' do
+    Steps 'for logging into the app and going to the profile page' do
+      Given 'that I am on the landing page' do
+        visit '/'
+      end
+      Then 'I can click on the sign up button' do
+        click_link 'Register'
+      end
+      And 'I fill in email and password fields' do
+        fill_in 'Email', with: 'food01@gmail.com'
+        fill_in 'Password', with: 'federico'
+        fill_in 'Password confirmation', with: 'federico'
+      end
+      And 'I submit the information' do
+         click_button 'Sign up'
+      end
+      Then 'I can click on the profile link' do
+        click_link 'Profile'
+      end
+      And 'I can see my email address' do
+        expect(page).to have_content "food01@gmail.com"
+      end
+    end
+  end
+end
