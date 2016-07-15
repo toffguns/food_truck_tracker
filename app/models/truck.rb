5dd617965d91320519bab9c83139fc446d189ba9
Michael Martinez <azaleabursts@gmail.com>
2016-07-14 17:07:11 -0700

    added image form to contributor partial in truck profile

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,7 +1,7 @@
 class Truck < ActiveRecord::Base
   validates :name, :cuisine, :menu, presence: true
   belongs_to :owner
-  has_many :time_and_places
+  has_many :time_and_places, :dependent => :delete_all
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment :image, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
