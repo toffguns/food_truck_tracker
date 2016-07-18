a4b8bf1f483d942e45ca2fc1350709a0e39a4504
eggplantzzz <pobol234@gmail.com>
2016-07-18 11:37:42 -0700

    removed register button when logged in as contributor

--- app/helpers/contributors_helper.rb
+++ app/helpers/contributors_helper.rb
@@ -1,2 +1,13 @@
 module ContributorsHelper
+
+  # Purpose: to determined if register link should be shown
+  # Signature: (Object, boolean) -> true/false
+  # Examples:
+  # show_register(nil, true) -> ..
+  # ...
+  # show_register(Owner.first, true) -> ...
+  # show_register(Owner.first, false) ->
+  def show_register#(current_owner, contributor_signed_in)
+    current_owner.nil? && !contributor_signed_in?
+  end
 end
